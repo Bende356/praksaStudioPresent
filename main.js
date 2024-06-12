36ae215187928dc6465ef3176d32b38ec484c7e4
Bende356 <134153040+Bende356@users.noreply.github.com>
2024-06-12 14:22:53 +0200

    Day3
    Sliders

---
+++ main.js
@@ -0,0 +1,22 @@
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+// Import Swiper core and required modules
+import Swiper, { Navigation, Pagination } from 'swiper';
+
+// Initialize Swiper
+const swiper = new Swiper('.swiper-container', {
+  modules: [Navigation, Pagination],
+  slidesPerView: 1,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
